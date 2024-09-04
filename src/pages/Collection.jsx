680c33ea6eaee0044eb06b1204672c89c03d3d49
digolstudiocode <dickydigol17@gmail.com>
2024-09-04 16:26:27 +0700

    penambahan pages collection

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,7 +1,102 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useContext, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import { assets } from "../assets/assets";
+import Title from "../components/Title";
+import ProductItem from "../components/ProductItem";
 
 const Collection = () => {
-  return <div>Collection</div>;
+  const { products } = useContext(ShopContext);
+  const [showFilter, setShowFilter] = useState(false);
+  const [filterProduct, setFilterProduct] = useState([]);
+
+  useEffect(() => {
+    setFilterProduct(products);
+  }, []);
+
+  return (
+    <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t ">
+      {/* Filter Options */}
+      <div className="min-w-60">
+        <p
+          onClick={() => setShowFilter(!showFilter)}
+          className="my-2 text-xl flex items-center cursor-pointer gap-2"
+        >
+          FILTERS
+          <img
+            src={assets.dropdown_icon}
+            alt=""
+            className={`h-3 sm:hidden ${showFilter ? "rotate-90" : ""}`}
+          />
+        </p>
+        {/* Categories Filter */}
+        <div
+          className={`border border-gray-300 pl-5 py-3 mt-6 ${
+            showFilter ? "" : "hidden"
+          } sm:block`}
+        >
+          <p className="mb-3 text-sm font-medium ">CATEGORIES</p>
+          <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
+            <p className="flex gap-2 ">
+              <input type="checkbox" className="w-3" value={"Men"} /> Men
+            </p>
+            <p className="flex gap-2 ">
+              <input type="checkbox" className="w-3" value={"Women"} /> Women
+            </p>
+            <p className="flex gap-2 ">
+              <input type="checkbox" className="w-3" value={"Kids"} /> Kids
+            </p>
+          </div>
+        </div>
+        {/* Sub-Category Filter */}
+        <div
+          className={`border border-gray-300 pl-5 py-3 my-6 ${
+            showFilter ? "" : "hidden"
+          } sm:block`}
+        >
+          <p className="mb-3 text-sm font-medium ">TYPE</p>
+          <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
+            <p className="flex gap-2 ">
+              <input type="checkbox" className="w-3" value={"Topwear"} />{" "}
+              Topwear
+            </p>
+            <p className="flex gap-2 ">
+              <input type="checkbox" className="w-3" value={"Bottomwear"} />{" "}
+              Bottomwear
+            </p>
+            <p className="flex gap-2 ">
+              <input type="checkbox" className="w-3" value={"Winterwear"} />{" "}
+              Winterwear
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* Right-Side */}
+      <div className="flex-1">
+        <div className="flex justify-between text-base sm:text-2xl mb-4 ">
+          <Title text1={"ALL"} text2={"COLLECTIONS"} />
+          {/* Product Sort */}
+          <select className="border-2 border-gray-300 px-2 text-sm">
+            <option value="relavent">Sort by: Relevant</option>
+            <option value="low-high">Sort by: Low to High</option>
+            <option value="high-low">Sort by: High to Low</option>
+          </select>
+        </div>
+        {/* Map Product */}
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-by-6">
+          {filterProduct.map((item, index) => (
+            <ProductItem
+              key={index}
+              id={item._id}
+              name={item.name}
+              price={item.price}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Collection;
