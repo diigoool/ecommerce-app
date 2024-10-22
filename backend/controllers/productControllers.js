64c0372f0d0dc0436e1621504f84b5fe364cd8de
digolstudiocode <dickydigol17@gmail.com>
2024-10-22 13:22:36 +0700

    7:47:25

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,22 +1,92 @@
+import { v2 as cloudinary } from "cloudinary";
+import productModel from "../models/productModel.js";
+
 // function for add product
 const addProduct = async (req, res) => {
-  res.send("Product Added");
+  try {
+    const {
+      name,
+      description,
+      price,
+      category,
+      subCategory,
+      sizes,
+      bestseller,
+    } = req.body;
+
+    const image1 = req.files.image1 && req.files.image1[0];
+    const image2 = req.files.image2 && req.files.image2[0];
+    const image3 = req.files.image3 && req.files.image3[0];
+    const image4 = req.files.image4 && req.files.image4[0];
+
+    const images = [image1, image2, image3, image4].filter(
+      (item) => item !== undefined
+    );
+
+    const imagesUrl = await Promise.all(
+      images.map(async (item) => {
+        let result = await cloudinary.uploader.upload(item.path, {
+          resource_type: "image",
+        });
+        return result.secure_url;
+      })
+    );
+
+    const productData = {
+      name,
+      description,
+      category,
+      price: Number(price),
+      subCategory,
+      bestseller: bestseller === "true" ? true : false,
+      sizes: JSON.parse(sizes),
+      images: imagesUrl,
+      date: Date.now(),
+    };
+
+    const product = new productModel(productData);
+    await product.save();
+
+    res.json({ success: true, message: "Product Added" });
+  } catch (error) {
+    console.log(error);
+    res.json({ succes: false, message: error.message });
+  }
 };
 
 // function for List Product
 const listProduct = async (req, res) => {
-  res.send("Product Added");
+  try {
+    const products = await productModel.find({});
+    res.json({ success: true, products: products });
+  } catch (error) {
+    console.log(error);
+    res.json({ succes: false, message: error.message });
+  }
 };
 
 //  function for removing product
 const removeProduct = async (req, res) => {
-  res.send("Product Added");
+  try {
+    await productModel.findByIdAndDelete(req.body.id);
+    res.json({ success: true, message: "Product Deleted" });
+  } catch (error) {
+    console.log(error);
+    res.json({ succes: false, message: error.message });
+  }
 };
 
 //   function for single product
 
 const singleProduct = async (req, res) => {
-  res.send("Product Added");
+  try {
+    const { productId } = req.body;
+    const product = await productModel.findById(productId);
+    res.json({ success: true, product });
+  } catch (error) {
+    console.log(error);
+    res.json({ succes: false, message: error.message });
+  }
 };
 
 export { addProduct, listProduct, removeProduct, singleProduct };
--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -84,7 +84,24 @@ const registerUser = async (req, res) => {
 
 // Routes for Admin Login
 const adminLogin = async (req, res) => {
-  res.send("Admin Logged In");
+  try {
+    const { email, password } = req.body;
+    if (
+      email === process.env.ADMIN_EMAIL &&
+      password === process.env.ADMIN_PASSWORD
+    ) {
+      const token = jwt.sign(email + password, process.env.JWT_SECRET);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Invalid Credentials" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.send({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 export { loginUser, registerUser, adminLogin };
