b33013eceef8582026788f969dfdf8a1031692a1
digolstudiocode <dickydigol17@gmail.com>
2024-10-23 18:28:26 +0700

    9:07:38

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { backendUrl } from "../App";
+import { toast } from "react-toastify";
 
 const List = () => {
-  return <div>List</div>;
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    try {
+      const response = await axios.get(backendUrl + "/api/product/list");
+
+      if (response.data.success) {
+        setList(response.data.products);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
 };
 
 export default List;
