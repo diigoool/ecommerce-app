6c4b0ff5fb6e2dfcc606fd9e459a01b2391ebdab
diigoool <dickydigol17@gmail.com>
2024-10-27 17:44:02 +0700

    10:33:12

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,9 +4,17 @@ const authUser = async (req, res, next) => {
   const { token } = req.headers;
 
   if (!token) {
-    return res.json({ success: false, message: "Unauthorized" });
+    return res.json({ success: false, message: "Unauthorized Login Again" });
   }
 
   try {
-  } catch (error) {}
+    const token_decode = jwt.verify(token, process.env.JWT_SECRET);
+    req.body.userId = token_decode.id;
+    next();
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
 };
+
+export default authUser;
--- backend/routes/cartRouter.js
+++ backend/routes/cartRouter.js
@@ -4,11 +4,12 @@ import {
   getUserCart,
   updateCart,
 } from "../controllers/cartControllers.js";
+import authUser from "../middleware/auth.js";
 
 const cartRouter = express.Router();
 
-cartRouter.post("/get", getUserCart);
-cartRouter.post("/add", addToCart);
-cartRouter.post("/update", updateCart);
+cartRouter.post("/get", authUser, getUserCart);
+cartRouter.post("/add", authUser, addToCart);
+cartRouter.post("/update", authUser, updateCart);
 
 export default cartRouter;
